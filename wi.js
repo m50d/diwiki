8b5c876229b7d67eacadf907c7621e5679491149
Michael Donaghy <md401@srcf.ucam.org>
2013-02-28 14:29:09 +0000

    More high level node hello world

--- wi.js
+++ wi.js
@@ -1,8 +1,13 @@
-var net = require('net');
+//var net = require('net');
 
-var server = net.createServer(function (socket) {
-  socket.write('Echo server\r\n');
-  socket.pipe(socket);
+var http = require('http');
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
 });
+//var server = net.createServer(function (socket) {
+//  socket.write('Echo server\r\n');
+//  socket.pipe(socket);
+//});
 
 server.listen(80, '127.0.0.1');
