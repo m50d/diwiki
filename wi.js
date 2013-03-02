7c6837b9abb1b6dc51065cf1980d584cc475e3d0
Michael Donaghy <md401@srcf.ucam.org>
2013-03-02 09:30:45 +0000

    Now load files from places on the filesystem

--- wi.js
+++ wi.js
@@ -1,21 +1,15 @@
-//var net = require('net');
-var sys   = require('sys');
-var    exec  = require('child_process').exec;
 var md = require("node-markdown").Markdown;
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
-var server = http.createServer(function(req, res){
-    fs.readFile("index.md", 'UTF-8', function(err, data) {
-var html = md(data, true);
-	 res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
-	});
-   
+var server = http.createServer(function(request, response){
+    var pathName = url.parse(request.url).pathname
+    fs.readFile('pages/' + pathName + '.md', 'UTF-8', function(err, data) {
+        var html = md(data, true);
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(html);
+    });
 });
-//var server = net.createServer(function (socket) {
-//  socket.write('Echo server\r\n');
-//  socket.pipe(socket);
-//});
 
 server.listen(80, '127.0.0.1');
