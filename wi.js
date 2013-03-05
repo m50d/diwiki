cf8bc450185ef068cf25c5fd5fe6fd025740e4b2
Michael Donaghy <md401@srcf.ucam.org>
2013-03-05 17:14:32 +0000

    Start work on a trivial templating system

--- wi.js
+++ wi.js
@@ -1,15 +1,20 @@
-var md = require("node-markdown").Markdown;
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var md = require("node-markdown").Markdown
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var cheerio = require('cheerio')
 
 var server = http.createServer(function(request, response){
     var pathName = url.parse(request.url).pathname
-    fs.readFile('pages/' + pathName + '.md', 'UTF-8', function(err, data) {
-        var html = md(data, true);
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end(html);
+    fs.readFile('./templates/view.html', 'UTF-8', function(err, templateData) {
+        fs.readFile('./pages/' + pathName + '.md', 'UTF-8', function(err, mddata) {
+	    var template = cheerio.load(templateData)
+            var content = md(mddata, true)
+            template('.content').html(content)
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.end(template.html())
+        });
     });
 });
 
-server.listen(80, '127.0.0.1');
+server.listen(80, '127.0.0.1')
