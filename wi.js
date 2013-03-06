018ea91e747a3f81e3fc4a783fdf57d3db856dbe
Michael Donaghy <md401@srcf.ucam.org>
2013-03-06 09:40:35 +0000

    Make templating more general. Start handling pages that don't exist

--- wi.js
+++ wi.js
@@ -1,20 +1,39 @@
-var md = require("node-markdown").Markdown
+var md = require('node-markdown').Markdown
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
 var cheerio = require('cheerio')
 
+function renderPage(templateName, substitutions, callback) {
+  fs.readFile('./templates/' + templateName + '.html', 'UTF-8', function(err, templateData) {
+    var template = cheerio.load(templateData)
+    for(var key in substitutions)
+      if(substitutions.hasOwnProperty(key)) {
+	template('.' + key).html(substitutions[key])
+      }
+    callback(template.html())
+  })
+}
+
 var server = http.createServer(function(request, response){
     var pathName = url.parse(request.url).pathname
-    fs.readFile('./templates/view.html', 'UTF-8', function(err, templateData) {
-        fs.readFile('./pages/' + pathName + '.md', 'UTF-8', function(err, mddata) {
-	    var template = cheerio.load(templateData)
-            var content = md(mddata, true)
-            template('.content').html(content)
+    var fileName = './pages/' + pathName + '.md'
+    fs.exists(fileName, function(pageExists) {
+      if(pageExists) {
+        fs.readFile(fileName, 'UTF-8', function(err, mddata) {
+          var content = md(mddata, true)
+          renderPage('view', {content: content}, function(html){
             response.writeHead(200, {'Content-Type': 'text/html'})
-            response.end(template.html())
-        });
-    });
-});
+            response.end(html)
+          })
+	})
+      } else {
+        renderPage('edit', {content: 'Enter *markdown* formatted text here'}, function(html){
+          response.writeHead(200, {'Content-Type': 'text/html'})
+          response.end(html)
+        })
+      }
+    })
+})
 
 server.listen(80, '127.0.0.1')
